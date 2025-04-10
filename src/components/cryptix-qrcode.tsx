0c30eae6fe5246e21987f460c9e47d69f50700fe
Cryptix Network <info@king-krypto.de>
2025-04-10 14:51:43 +0200

    fix

--- src/components/cryptix-qrcode.tsx
+++ src/components/cryptix-qrcode.tsx
@@ -4,16 +4,16 @@ export default function CryptixQrCode(props) {
     return (
         <QRCode
             value={props.value}
-            fgColor='#134a40'
+            fgColor='#0081d4'
             eyeRadius={[
                 [10, 10, 0, 10], // top/left eye
                 [10, 10, 10, 0], // top/right eye
                 [10, 0, 10, 10], // bottom/left
             ]}
             eyeColor={[
-                { outer: '#134a40', inner: '#49EACB' },
-                { outer: '#134a40', inner: '#49EACB' },
-                { outer: '#134a40', inner: '#49EACB' },
+                { outer: '#0081d4', inner: '#00bcd4' },
+                { outer: '#0081d4', inner: '#00bcd4' },
+                { outer: '#0081d4', inner: '#00bcd4' },
             ]}
         />
     );
